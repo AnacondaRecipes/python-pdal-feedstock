753b061b77d34de3a1de6aea43d0940bd81cb05b
ViridianMelody <mlove@anaconda.com>
2023-08-22 11:19:49 -0600

    Remove duplicate activate scripts on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,17 +12,3 @@ if errorlevel 1 exit 1
 
 copy %RECIPE_DIR%\scripts\deactivate.bat %DEACTIVATE_DIR%\pdal-python-deactivate.bat
 if errorlevel 1 exit 1
-
-:: Copy unix shell activation scripts, needed by Windows Bash users
-copy %RECIPE_DIR%\scripts\activate.sh %ACTIVATE_DIR%\pdal-python-activate.sh
-if errorlevel 1 exit 1
-
-copy %RECIPE_DIR%\scripts\deactivate.sh %DEACTIVATE_DIR%\pdal-python-deactivate.sh
-if errorlevel 1 exit 1
-
-:: Copy powershell activation scripts
-copy %RECIPE_DIR%\scripts\activate.ps1 %ACTIVATE_DIR%\pdal-python-activate.ps1
-if errorlevel 1 exit 1
-
-copy %RECIPE_DIR%\scripts\deactivate.ps1 %DEACTIVATE_DIR%\pdal-python-deactivate.ps1
-if errorlevel 1 exit 1
